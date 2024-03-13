ce4876347fb41e8b6c587352f6457ae9da5c4753
Yalbas <doriandealba@colegioguanenta.edu.co>
2024-03-13 07:53:53 -0500

    Se realizo analisis y diseño del quizz

--- README.md
+++ README.md
@@ -4,6 +4,31 @@
 ### Realice el análisis, diseño y construcción para resolver el siguiente problema:
 
 Dados tres números a, b y c, correspondientes a la longitud de los lados de una figura geométrica, determinar si pueden formar los lados de un triángulo.
+# Analisis 
+
+--Variables de entrada
+
+
+a= Ingresar la longitud de uno de los lados
+b= Ingresar la longitud de uno de los lados
+c=  Ingresar la longitud de uno de los lados
+
+--variables de proceso
+
+
+a + b > c:
+b + c > a:
+c + a > b:
+
+Para que pueda ser un triangulo
+
+--Variables de salida
+
+
+Resultado= Puede o no ser un triangulo con los angulos dados
+
+
+--
 
 ## DISEÑO
 
--- quiz.py
+++ quiz.py
@@ -1,12 +1,22 @@
 # Quiz instrucciones condicionales
 """Dados tres números a, b y c, correspondientes a la longitud de los lados de una figura geométrica, determinar si pueden formar los lados de un triángulo."""
 
-print("------------------------------")
-print("----- VERIFICAR TRIANGULO ----")
-print("------------------------------")
-
-# input
+# Input
+a = float(input("Digite el valor del vertice 1: "))
+b = float(input("Digite el valor del vertice 2: "))
+c = float(input("Digite el valor del vertice 3: "))
 
 # processing
 
-# output
+if a + b > c:
+    Resultado = "Se puede utilizar como un triangulo"
+    if b + c > a:
+        Resultado = "Se puede utilizar como un triangulo"
+        if c + a > b:
+            Resultado = "Se puede utilizar como un triangulo"
+        else:
+            Resultado = "No se puede utilizar como un triangulo"
+
+# output 
+
+print("Los lados", a, ",", b, "y", c, Resultado )
